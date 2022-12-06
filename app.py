6ebd473c8d9cbfacc89855b5bee7debcbcccc73e
rezaaebraahimi <rezaebrahimiproducton@gmail.com>
2022-12-06 15:32:28 +0330

    update-1.0.2

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import time
 import psutil
 import speedtest
@@ -18,38 +19,41 @@ def bandwidth():
         MB_upload = (upload_byte - upload_band) / 1024**2
         MB_total = (total_byte - total_band) / 1024**2
         
-        print (f"{MB_download:.3f} MB Download, {MB_upload:.3f} MB Upload, {MB_total:.2f} MB Total")
+        status = print (f"{MB_download:.3f} MB Download, {MB_upload:.3f} MB Upload, {MB_total:.2f} MB Total")
         
         download_band = download_byte
         upload_band = upload_byte
         total_band = total_byte
         
         time.sleep(1)
-
-
+        return status
 
 
 def speed_test():
     
     test = speedtest.Speedtest()
+    
     print("Loading server list...")
     test.get_servers()
+    
     print("Choosing best server...")
     best = test.get_best_server()
+    
     print(f"Name: {best['name']} - Host: {best['host']} - Country: {best['country']}({best['cc']}) - Latency: {best['latency']}")
     
     print("Download test speed..")
     download_result = (test.download()) / 1024**2
     print(f"Download speed: {download_result:.3f} Mbit/s")
+    
     print("Upload test speed..")
     upload_result = (test.upload()) / 1024**2
     print(f"Upload speed: {upload_result:.3f} Mbit/s")
+    
     print("Ping test..")
     ping_result = test.results.ping
     print(f"Ping: {ping_result:.3f} ms")
     
     
-    
-    
-    
-speed_test()
+if __name__ == "__main__":
+    while True:
+        bandwidth()
