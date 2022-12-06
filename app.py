17d48829ed10e9deaeae8ec63f2f19beb59bfab4
rezaaebraahimi <rezaebrahimiproducton@gmail.com>
2022-12-06 18:56:48 +0330

    update-1.0.4

--- app.py
+++ app.py
@@ -1,28 +1,26 @@
 import time
 import psutil
 import speedtest
+import socket
+import requests
 
 
 def bandwidth():
     
     download_band = psutil.net_io_counters().bytes_recv
     upload_band = psutil.net_io_counters().bytes_sent
-    total_band = download_band + upload_band
     
     while True:
         download_byte = psutil.net_io_counters().bytes_recv
         upload_byte = psutil.net_io_counters().bytes_sent
-        total_byte = download_band + upload_band
         
         MB_download = (download_byte - download_band) / 1024**2
         MB_upload = (upload_byte - upload_band) / 1024**2
-        MB_total = (total_byte - total_band) / 1024**2
         
-        status = print (f"{MB_download:.3f} MB Download, {MB_upload:.3f} MB Upload, {MB_total:.2f} MB Total")
+        status = print (f"Download: {MB_download:.3f} MB - Upload: {MB_upload:.3f} MB")
         
         download_band = download_byte
         upload_band = upload_byte
-        total_band = total_byte
         
         time.sleep(1)
         return status
@@ -35,7 +33,7 @@ def speed_test():
     print("Loading server list...")
     test.get_servers()
     
-    print("Choosing best server...")
+    print("Find your server...")
     best = test.get_best_server()
     
     print(f"Name: {best['name']} - Host: {best['host']} - Country: {best['country']}({best['cc']}) - Latency: {best['latency']}")
@@ -47,22 +45,48 @@ def speed_test():
     print("Upload test speed..")
     upload_result = (test.upload()) / 1024**2
     print(f"Upload speed: {upload_result:.3f} Mbit/s")
+
     
-    print("Ping test..")
-    ping_result = test.results.ping
-    print(f"Ping: {ping_result:.3f} ms")
-    
+def ping():
+    test = speedtest.Speedtest()
+    test.get_best_server()
+    ping_test = test.results.ping
+    ping_result = print(f"Ping: {ping_test:.2f} ms")
+    return ping_result
+     
     
 def usage():
-    while True:
-        cpu_usage = psutil.cpu_percent()
-        memory_usage = psutil.virtual_memory().percent
-        status_2 = print (f"CPU: {cpu_usage}% - Memory: {memory_usage}%") 
-        time.sleep(1)
-        return status_2
+    cpu_usage = psutil.cpu_percent()
+    memory_usage = psutil.virtual_memory().percent
+    status_2 = print (f"CPU: {cpu_usage}% - Memory: {memory_usage}%") 
+    time.sleep(1)
+    return status_2
+
+
+def route_ip():
+    route_address = socket.gethostbyname(socket.gethostname())
+    return route_address
+
+
+def get_ip():
+    response = requests.get('https://api64.ipify.org?format=json').json()
+    return response["ip"]
+
+
+def get_location():
+    ip_address = get_ip()
+    response = requests.get(f'https://ipapi.co/{ip_address}/json/').json()
+    location_data = {"ip": ip_address,
+                    "city": response.get("city"),
+                    "region": response.get("region"),
+                    "country": response.get("country_name")}
+    return location_data
+
+
 
 if __name__ == "__main__":
     while True:
         bandwidth()
         usage()
+        
         
